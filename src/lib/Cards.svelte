b5ac59c75a026c44ab690bba979c5db5d3ebc18c
ConnorMcGehee <connorjames050@gmail.com>
2023-07-26 09:08:27 -0500

    Updated loop logic

---
+++ src/lib/Cards.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import Card from "./Card.svelte";
+    import { websites } from "./stores";
+</script>
+
+<div id="cards">
+    {#each $websites as website, index (website.id)}
+        <Card {website} {index} />
+    {/each}
+</div>
+
+<style>
+    #cards {
+        margin-top: 1rem;
+        position: relative;
+    }
+</style>
