082b8eeca3eac602fa1dfc8451f0091f9991844a
ConnorMcGehee <connorjames050@gmail.com>
2023-07-27 11:53:46 -0500

    Changed to static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,8 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapterStatic from '@sveltejs/adapter-static';
+
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,11 +11,11 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+        adapter: adapterStatic(),
+        paths: {
+            base: dev ? '' : process.env.BASE_PATH,
+        }
+    }
 };
 
 export default config;
