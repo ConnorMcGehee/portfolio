148f8e7a62f17e587acd3b7f0c681404e07815df
ConnorMcGehee <connorjames050@gmail.com>
2023-07-27 16:39:32 -0500

    Tidied up code to improve readability

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -9,52 +9,40 @@
 
     export let website: import("./types").Website;
     export let index: number;
-    let isDragged = false;
+
+    const THRESHOLD = 75;
+    let position = { x: 0, y: 0 };
     let opacity = 1;
-    let initialized = false;
-
     let swipeClass: string | null = null;
-
+    let isDragged = false;
+    let dragOptions: DragOptions;
     let isTopCard = false;
 
     $: isTopCard = index === $websites.length - 1;
-
-    const THRESHOLD = 75;
-
-    let position = { x: 0, y: 0 };
-
-    let dragOptions: DragOptions;
-
     $: dragOptions = {
         axis: "x",
         legacyTranslate: false,
-        position: position,
-        onDrag: onDrag,
-        onDragEnd: onDragEnd,
+        position,
+        onDrag,
+        onDragEnd,
         disabled: !isTopCard,
     };
 
-    function onDrag(data: DragEventData) {
-        const x = data.offsetX;
+    function onDrag({ offsetX: x }: DragEventData) {
         getX.set(x);
-        if (x < 0 || x > 0) {
-            isDragged = true;
-        } else {
-            isDragged = false;
-        }
-
+        isDragged = x !== 0;
         opacity = 1 - Math.abs(x / 375);
     }
 
-    async function onDragEnd() {
-        let isSwiped = Math.abs($getX) > THRESHOLD;
-
-        if ($getX < -THRESHOLD) {
-            swipeClass = "swipe-left";
-        } else if ($getX > THRESHOLD) {
-            swipeClass = "swipe-right";
-        }
+    function onDragEnd() {
         isDragged = false;
+        const isSwiped = Math.abs($getX) > THRESHOLD;
+        swipeClass =
+            $getX < -THRESHOLD
+                ? "swipe-left"
+                : $getX > THRESHOLD
+                ? "swipe-right"
+                : null;
         if (isSwiped) {
             setPositionX($getX);
             nextCard();
@@ -64,34 +52,29 @@
         }
     }
 
-    function nextCard() {
-        initialized = false;
-        getX.set(0);
-        websites.update((websites) => {
-            const swipedWebsite = websites.splice(index, 1)[0];
-            websites.unshift(swipedWebsite);
-            return websites;
-        });
+    function setPositionX(x: number) {
+        position = { ...position, x };
+        getX.set(x);
     }
 
-    function setPositionX(x: number) {
-        position = { ...position, x: x };
-        getX.set(x);
+    function nextCard() {
+        getX.set(0);
+        websites.update((websiteCopy) => {
+            websiteCopy.unshift(websiteCopy.splice(index, 1)[0]);
+            return websiteCopy;
+        });
     }
 
     const rotation = spring(0, { stiffness: 0.08, damping: 0.3 });
 
     $: {
         let normalizedX = Math.abs($getX) / THRESHOLD;
-        let degrees;
         switch (index) {
             case $websites.length - 2:
-                degrees = -7 * (1 - normalizedX);
-                rotation.set(degrees <= 0 ? degrees : 0);
+                rotation.set(Math.min(-7 * (1 - normalizedX), 0));
                 break;
             case $websites.length - 3:
-                degrees = 7 * (1 - normalizedX * 2);
-                rotation.set(degrees >= -7 ? degrees : -7);
+                rotation.set(Math.max(7 * (1 - normalizedX * 2), -7));
                 break;
             default:
                 rotation.set(0);
@@ -99,7 +82,7 @@
     }
 
     function onClick(e: MouseEvent) {
-        if ($getX === 0 || !isTopCard) {
+        if (!isTopCard || $getX === 0) {
             e.preventDefault();
         }
     }
@@ -111,11 +94,12 @@
         draggable="false"
         class="card {swipeClass}"
         class:behind={!isTopCard}
-        class:dragged={isDragged}
         src={website.imageSrc}
-        alt="Screenshot of ${website.title}"
+        alt="Screenshot of {website.title}"
         style="rotate: {$rotation}deg;
-            opacity: {index < $websites.length - 3 && !swipeClass ? 0 : opacity};
+            opacity: {index < $websites.length - 3 && !swipeClass
+            ? 0
+            : opacity};
             z-index: {swipeClass ? $websites.length : index};"
         on:animationend={() => {
             setPositionX(0);
@@ -137,7 +121,6 @@
         border: solid white 0.2rem;
         position: absolute;
         translate: -50%;
-        transition: transform 0.2s ease-in-out;
         -webkit-user-select: none;
         user-select: none;
     }
@@ -145,10 +128,6 @@
     .behind {
         cursor: default;
         pointer-events: none;
-    }
-
-    .dragged {
-        transition: none;
     }
 
     @keyframes swipeLeft {
@@ -166,14 +145,10 @@
     }
 
     .swipe-left {
-        transition: none;
-        z-index: 9999;
         animation: swipeLeft 0.3s ease-out forwards;
     }
 
     .swipe-right {
-        transition: none;
-        z-index: 9999;
         animation: swipeRight 0.3s ease-out forwards;
     }
 </style>
