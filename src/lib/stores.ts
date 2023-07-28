4f4d36bcad7afd9588ac88a4019fa772a8505cbf
ConnorMcGehee <connorjames050@gmail.com>
2023-07-28 07:53:56 -0500

    Added project descriptions

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -11,25 +11,33 @@ import type { Project } from './types';
 const playvert: Project = {
     title: "Playvert",
     imageSrc: playvertImage,
-    url: "https://playvert.com"
+    url: "https://playvert.com",
+    repo: "https://github.com/ConnorMcGehee/Playvert",
+    description: "Playvert is a versatile web application designed for sharing playlists across multiple music streaming platforms."
 };
 
 const breweryLocator: Project = {
     title: "Brewery Locator",
     imageSrc: breweryLocatorImage,
-    url: "https://connormcgehee.github.io/Brewery-Locator/"
+    url: "https://connormcgehee.github.io/Brewery-Locator/",
+    repo: "https://github.com/ConnorMcGehee/Brewery-Locator",
+    description: "Brewery Locator uses the Open Brewery DB API to locate and display the nearest breweries to the user."
 };
 
 const mastermind: Project = {
     title: "Mastermind",
     imageSrc: mastermindImage,
-    url: "https://connormcgehee.github.io/Mastermind/"
+    url: "https://connormcgehee.github.io/Mastermind/",
+    repo: "https://github.com/ConnorMcGehee/Mastermind",
+    description: "Mastermind is a JavaScript game based off of the Israelian code-breaking board game. Written in vanilla JS, CSS, and HTML."
 };
 
 const towersOfHanoi: Project = {
     title: "Towers of Hanoi",
     imageSrc: towersOfHanoiImage,
-    url: "https://connormcgehee.github.io/TowersOfHanoi/"
+    url: "https://connormcgehee.github.io/TowersOfHanoi/",
+    repo: "https://github.com/ConnorMcGehee/TowersOfHanoi",
+    description: "Towers of Hanoi is a JavaScript game based off the popular mathematical puzzle. Written in vanilla JS, CSS, and HTML."
 };
 
 export const projects = writable(
