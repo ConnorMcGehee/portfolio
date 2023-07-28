30565752c8749c00503b0525ecb5c90b5202047c
ConnorMcGehee <connorjames050@gmail.com>
2023-07-28 00:53:39 -0500

    Added love and dislike buttons

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { websites, getX } from "./stores";
+    import { projects, getX, swipeEvent } from "./stores";
     import {
         draggable,
         type DragEventData,
@@ -7,18 +7,18 @@
     } from "./drag-core";
     import { spring } from "svelte/motion";
 
-    export let website: import("./types").Website;
+    export let project: import("./types").Project;
     export let index: number;
 
     const THRESHOLD = 75;
     let position = { x: 0, y: 0 };
-    let opacity = 1;
+    let opacity = spring(1, { stiffness: 0.08, damping: 0.5 });
     let swipeClass: string | null = null;
     let isDragged = false;
     let dragOptions: DragOptions;
     let isTopCard = false;
 
-    $: isTopCard = index === $websites.length - 1;
+    $: isTopCard = index === $projects.length - 1;
     $: dragOptions = {
         axis: "x",
         legacyTranslate: false,
@@ -31,23 +31,18 @@
     function onDrag({ offsetX: x }: DragEventData) {
         getX.set(x);
         isDragged = x !== 0;
-        opacity = 1 - Math.abs(x / 375);
+        opacity.set(1 - Math.abs(x / 375));
     }
 
     function onDragEnd() {
         isDragged = false;
         const isSwiped = Math.abs($getX) > THRESHOLD;
-        swipeClass =
-            $getX < -THRESHOLD
-                ? "swipe-left"
-                : $getX > THRESHOLD
-                ? "swipe-right"
-                : null;
         if (isSwiped) {
+            const direction = $getX < -THRESHOLD ? "left" : "right";
             setPositionX($getX);
-            nextCard();
+            swipe(direction);
         } else {
-            opacity = 1;
+            opacity.set(1);
             setPositionX(0);
         }
     }
@@ -57,11 +52,24 @@
         getX.set(x);
     }
 
+    function swipe(direction: string) {
+        swipeEvent.set("");
+        swipeClass = `swipe-${direction}`;
+        opacity.set(0);
+        nextCard();
+    }
+
+    $: {
+        if ($swipeEvent && isTopCard) {
+            swipe($swipeEvent);
+        }
+    }
+
     function nextCard() {
         getX.set(0);
-        websites.update((websiteCopy) => {
-            websiteCopy.unshift(websiteCopy.splice(index, 1)[0]);
-            return websiteCopy;
+        projects.update((projectsCopy) => {
+            projectsCopy.unshift(projectsCopy.splice(index, 1)[0]);
+            return projectsCopy;
         });
     }
 
@@ -70,10 +78,10 @@
     $: {
         let normalizedX = Math.abs($getX) / THRESHOLD;
         switch (index) {
-            case $websites.length - 2:
+            case $projects.length - 2:
                 rotation.set(Math.min(-7 * (1 - normalizedX), 0));
                 break;
-            case $websites.length - 3:
+            case $projects.length - 3:
                 rotation.set(Math.max(7 * (1 - normalizedX * 2), -7));
                 break;
             default:
@@ -88,23 +96,21 @@
     }
 </script>
 
-<a draggable="false" target="_blank" href={website.url} on:click={onClick}>
+<a draggable="false" target="_blank" href={project.url} on:click={onClick}>
     <img
         use:draggable={dragOptions}
         draggable="false"
         class="card {swipeClass}"
         class:behind={!isTopCard}
-        src={website.imageSrc}
-        alt="Screenshot of {website.title}"
+        src={project.imageSrc}
+        alt="Screenshot of {project.title}"
         style="rotate: {$rotation}deg;
-            opacity: {index < $websites.length - 3 && !swipeClass
-            ? 0
-            : opacity};
-            z-index: {swipeClass ? $websites.length : index};"
+            opacity: {$opacity};
+            z-index: {swipeClass ? $projects.length : index};"
         on:animationend={() => {
             setPositionX(0);
             setTimeout(() => {
-                opacity = 1;
+                opacity.set(1);
                 swipeClass = null;
             }, 100);
         }}
@@ -120,7 +126,6 @@
         border-radius: 0.5rem;
         border: solid white 0.2rem;
         position: absolute;
-        translate: -50%;
         -webkit-user-select: none;
         user-select: none;
     }
@@ -132,15 +137,13 @@
 
     @keyframes swipeLeft {
         100% {
-            translate: calc(-10rem - 50%);
-            opacity: 0;
+            translate: -10rem;
         }
     }
 
     @keyframes swipeRight {
         100% {
-            translate: calc(10rem - 50%);
-            opacity: 0;
+            translate: 10rem;
         }
     }
 
--- src/lib/Cards.svelte
+++ src/lib/Cards.svelte
@@ -1,17 +1,163 @@
 <script lang="ts">
     import Card from "./Card.svelte";
-    import { websites } from "./stores";
+    import { projects, swipeEvent } from "./stores";
+    import Fa from "svelte-fa";
+    import { faXmark, faHeart } from "@fortawesome/free-solid-svg-icons";
+    import { spring } from "svelte/motion";
+    import { fade } from "svelte/transition";
+
+    function swipe(direction: string) {
+        swipeEvent.set(direction);
+    }
+
+    const dislikeSize = spring(
+        { width: 3.5, font: 2 },
+        { stiffness: 0.1, damping: 0.3 }
+    );
+    const loveSize = spring(
+        { width: 3.5, font: 1.65 },
+        { stiffness: 0.1, damping: 0.3 }
+    );
+
+    function dislikeClick() {
+        swipe("left");
+    }
+
+    function loveClick() {
+        swipe("right");
+    }
 </script>
 
-<div id="cards">
-    {#each $websites as website, index (website.id)}
-        <Card {website} {index} />
-    {/each}
+<div id="projects">
+    <div id="cards-container">
+        {#each $projects as project, index (project.url)}
+            <Card {project} {index} />
+        {/each}
+    </div>
+    {#key $projects[$projects.length - 1].title}
+        <a
+            href={$projects[$projects.length - 1].url}
+            target="_blank"
+            class="title"
+            transition:fade={{ duration: 150 }}
+        >
+            <h2>{$projects[$projects.length - 1].title}</h2>
+        </a>
+    {/key}
+
+    <button
+        on:click={() => dislikeClick()}
+        class="dislike icon"
+        on:mouseover={() => dislikeSize.set({ width: 4.25, font: 2.75 })}
+        on:focus={() => dislikeSize.set({ width: 4.25, font: 2.75 })}
+        on:mouseleave={() => dislikeSize.set({ width: 3.5, font: 2 })}
+        on:blur={() => dislikeSize.set({ width: 3.5, font: 2 })}
+        style="width: {$dislikeSize.width}rem;
+        font-size: {$dislikeSize.font}rem;"
+    >
+        <Fa icon={faXmark} />
+    </button>
+    <button
+        on:click={() => loveClick()}
+        class="love icon"
+        on:mouseover={() => loveSize.set({ width: 4.25, font: 2.4 })}
+        on:focus={() => loveSize.set({ width: 4.25, font: 2.4 })}
+        on:mouseleave={() => loveSize.set({ width: 3.5, font: 1.65 })}
+        on:blur={() => loveSize.set({ width: 3.5, font: 1.65 })}
+        style="width: {$loveSize.width}rem;
+        font-size: {$loveSize.font}rem;"
+    >
+        <Fa icon={faHeart} />
+    </button>
 </div>
 
 <style>
-    #cards {
+    :root {
+        --love-color: rgb(255, 195, 228);
+        --dislike-color: rgb(255, 64, 3);
+    }
+
+    #projects {
+        display: grid;
+        grid-template-areas:
+            "card card"
+            "title title"
+            "dislike love";
+        column-gap: 25vw;
+        row-gap: 1rem;
+        justify-items: center;
         margin-top: 1rem;
+        max-width: 70rem;
+    }
+
+    #cards-container {
         position: relative;
+        grid-area: card;
+        width: 72vw;
+        max-width: 17rem;
+        max-height: 58dvh;
+        aspect-ratio: 9/16;
+    }
+
+    .title {
+        grid-area: title;
+    }
+
+    .title h2 {
+        font-weight: normal;
+    }
+
+    .icon {
+        cursor: pointer;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 3.5rem;
+        border-radius: 50%;
+        border: solid 0.2rem;
+        aspect-ratio: 1;
+        padding: 0.5rem;
+        -webkit-user-select: none;
+        user-select: none;
+        z-index: 9999;
+        background: none;
+    }
+
+    .dislike {
+        font-size: 2rem;
+        color: var(--dislike-color);
+        border-color: var(--dislike-color);
+        box-shadow: 0px 0px 20px 0px var(--dislike-color);
+        grid-area: dislike;
+        justify-self: end;
+    }
+
+    .love {
+        font-size: 1.65rem;
+        color: var(--love-color);
+        border-color: var(--love-color);
+        box-shadow: 0px 0px 20px 0px var(--love-color);
+        grid-area: love;
+        justify-self: start;
+    }
+
+    .love,
+    .dislike {
+        align-self: center;
+    }
+
+    a,
+    a:visited {
+        color: inherit;
+        text-decoration: none;
+    }
+
+    @media (min-width: 31.25rem) {
+        #projects {
+            grid-template-areas:
+                "dislike card love"
+                ". title .";
+            column-gap: 2rem;
+        }
     }
 </style>
